a5f67a79612ae92bde0d4bb20db54ee923396612
GarrettCooper301 <garrettcooper301@gmail.com>
2023-06-22 14:03:30 -0600

    completed lesson 10 assignment

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5861897&q=Fairbanks&units=imperial&appid=fef3472ec1bd82edd28da3682f86f84e";
+fetch(apiURL)
+	.then((response) => response.json())
+	.then((jsObject) => {
+		console.log(jsObject);
+		document.querySelector("#current-temp").textContent =
+			jsObject.main.temp;
+
+		const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+		const desc = jsObject.weather[0].description;
+
+		document.querySelector("#weather-icon").setAttribute("src", iconsrc);
+		document.querySelector("#weather-icon").setAttribute("alt", desc);
+		document.querySelector("#caption-desc").textContent = desc;
+	});
